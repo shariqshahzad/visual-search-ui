f9e407f5756a28a052376319c61ac08824857aaf
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2021-10-22 22:37:26 +0500

    vue js app initiated

---
+++ src/services/bingSearch.service.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export default {
+  async getResults(imageUrl) {
+    const bodyFormData = new FormData();
+    bodyFormData.append(
+      "knowledgeRequest",
+      JSON.stringify({
+        imageInfo: {
+          url: `${imageUrl}`,
+        },
+        knowledgeRequest: {
+          filters: { site: "https://www.williams-sonoma.com" },
+        },
+      })
+    );
+    const headers = {
+      "Content-Type": `multipart/form-data; boundary=${bodyFormData._boundary}`,
+      "Ocp-Apim-Subscription-Key": "691fac28b4e145bdb4ca086f6c190c5d",
+    };
+    console.log(bodyFormData);
+    let res = await axios({
+      method: "post",
+      url: "https://api.bing.microsoft.com/v7.0/images/visualsearch?mkt=en-us",
+      data: bodyFormData,
+      headers: headers,
+    });
+
+    console.log(res.data);
+    return res.data;
+  },
+};
