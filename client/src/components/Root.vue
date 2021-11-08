73e9b687e2ed76f6bf410832e60db5432d9c4e71
Muhammad Ahmed <MAhmed3@wsgc.com>
2021-11-08 07:03:22 -0800

    CORs issue has been sorted.

--- client/src/components/Root.vue
+++ client/src/components/Root.vue
@@ -137,6 +137,11 @@ export default {
       errorDetail: "",
     };
   },
+  computed:{
+    imageProxyUrl:function (){
+      return `${process.env.VUE_APP_PROXY_SERVER_URL}/${this.imageUrl}`;
+    }
+  },
   methods: {
     // onChangeInputSelection() {
     //   console.log(this.radioGrp);
@@ -169,7 +174,7 @@ export default {
               isUrl: this.radioGrp === "imageUrl",
               src:
                 this.radioGrp === "imageUrl"
-                  ? this.imageUrl
+                  ? this.imageProxyUrl
                   : URL.createObjectURL(this.files),
             };
             this.dialog = true;
