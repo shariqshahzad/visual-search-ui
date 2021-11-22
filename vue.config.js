1dc26596fe36e1a245f78655d86eba48e4f59c53
Muhammad Ahmed <MAhmed3@wsgc.com>
2021-11-22 01:18:46 -0800

    Changed page title.

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
   pages: {
     index: {
       entry: 'client/src/main.js',
-      template: 'client/public/index.html'
+      template: 'client/public/index.html',
+      title: 'Visual Search',
     }
   }
 }
