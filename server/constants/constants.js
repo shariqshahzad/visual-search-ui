67082231bc8edbf3f1807ceac49247763be99120
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2022-02-18 18:13:55 +0500

    api url changed

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-  WSI_ML_IMAGE_SEARCH_URL: `http://4bf18615-506a-42a6-ac60-6013ce82b5b1.eastus.azurecontainer.io/score`,
+  WSI_ML_IMAGE_SEARCH_URL: `http://4a4b5e38-733b-4dba-b887-235138846823.eastus.azurecontainer.io/score`,
   WSI_ML_IMAGE_YOLO_URL: `http://3a5fda01-4e03-4f98-8e1f-b43d823b5616.eastus.azurecontainer.io/score`,
-  WSI_ML_IMAGE_SEARCH_API_KEY: `sNtjg4v5MjyyHeaSMEc7kX7gDVgKaOYb`,
+  WSI_ML_IMAGE_SEARCH_API_KEY: `4cWBSfbx6UyUmpGhRZHyIdtra1lFmEJW`,
   WSI_ML_IMAGE_YOLO_API_KEY: `IDZwR8awU1HtNvRW34g1a6fSG9bIccnx`,
 });
