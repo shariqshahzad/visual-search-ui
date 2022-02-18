926cb561dc8527c37ff19a1aacde5eb094fe1a76
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2022-02-18 19:01:33 +0500

    view more fixed

--- client/src/components/CategoryProductDisplay.vue
+++ client/src/components/CategoryProductDisplay.vue
@@ -2,13 +2,21 @@
   <div>
     <v-row v-for="(category, index) in data" :key="index">
       <v-col cols="12">
-        <v-sheet class="pa-2" elevation="8" max-width="1100" style="overflow:hidden">
+        <v-sheet
+          class="pa-2"
+          elevation="8"
+          max-width="1100"
+          style="overflow: hidden"
+        >
           <div>
-
-            <span cols="10" class="ml-5 text-h6 font-weight-medium text-capitalize">
+            <span
+              cols="10"
+              class="ml-5 text-h6 font-weight-medium text-capitalize"
+            >
               {{ category.categoryName }} :</span
             >
-            <a cols="12"
+            <a
+              cols="12"
               class="
                 mr-5
                 text-h6
@@ -49,13 +57,17 @@
         </v-toolbar>
         <div style="padding: 10px; overflow: hidden">
           <v-row class="mt-5">
-            <v-col
-              cols="3"
+            <ProductCard
               v-for="(product, index) in selectedCategoryFullData"
               :key="index"
+              :product="product"
+            />
+            <!-- <v-col
+              cols="3"
+
             >
-              <ProductCard :product="product" />
-            </v-col>
+              
+            </v-col> -->
           </v-row>
         </div>
       </v-card>
@@ -64,9 +76,9 @@
 </template>
 <style scoped>
 .slider-bar {
-    flex-wrap: nowrap !important;
-    overflow-x: scroll !important;
-    display: flex;
+  flex-wrap: nowrap !important;
+  overflow-x: scroll !important;
+  display: flex;
 }
 </style>
 <script>
