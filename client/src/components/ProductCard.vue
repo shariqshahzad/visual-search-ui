499476c47fcaa3f415e05762a14683388fd6a07b
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2022-04-18 23:08:51 +0500

    pid added to product card

--- client/src/components/ProductCard.vue
+++ client/src/components/ProductCard.vue
@@ -1,11 +1,20 @@
 <template>
-  <v-card class="ma-2" height="260" width="200">
+  <v-card class="ma-2" height="300" width="200">
     <v-img :src="product.image" height="160" max-width="300"></v-img>
     <v-card-text>
       <div class="text-subtitle-1">
         {{ brands[product.brand].name }}
       </div>
       <div>SKU ID: {{ product.skuid }}</div>
+      <v-tooltip v-if="product.pid.length > 33" bottom>
+        <template v-slot:activator="{ on, attrs }">
+          <span v-bind="attrs" v-on="on"
+            >PID: {{ product.pid.substring(0, 33) }} ...</span
+          >
+        </template>
+        <span>{{ product.pid }}</span>
+      </v-tooltip>
+      <div v-else>PID: {{ product.pid }}</div>
       <div>Similarity Score: {{ product.similarity.toFixed(2) }}</div>
     </v-card-text>
   </v-card>
