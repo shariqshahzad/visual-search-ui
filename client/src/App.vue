9b1a7a1f7508ab02a26e92045a3374ffed909af7
Shariq Shahzad <shariq_shahzad07@yahoo.com>
2022-07-04 22:57:06 +0500

    maintenance page deployment

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <v-app>
-      <Root/>
+    <MaintenancePage/>
+      <!-- <Root/> -->s
   </v-app>
 </template>
 
 <script>
 import Root from './components/Root';
+import MaintenancePage from './components/MaintenancePage'
 
 export default {
   name: 'App',
 
   components: {
     Root,
+    MaintenancePage
   },
 
   data: () => ({
