235906d9f81f8c1371569d03741aa14f85e9e61d
Muhammad Ahmed <MAhmed3@wsgc.com>
2022-07-07 07:39:21 -0700

    Changed similarity cluster url and key

--- server/constants/constants.js
+++ server/constants/constants.js
@@ -2,8 +2,8 @@ module.exports = Object.freeze({
   WSI_ML_IMAGE_EMBEDDINGS_URL: `http://20.81.26.170:80/api/v1/service/emb-dev-cluster/score`,
   WSI_ML_IMAGE_EMBEDDINGS_API_KEY: `tTVGggXQzSvP05akpcLoYaCPfEuKRz8E`,
 
-  WSI_ML_IMAGE_SEARCH_URL: `http://20.124.59.72:80/api/v1/service/sim-dev-cluster/score`,
-  WSI_ML_IMAGE_SEARCH_API_KEY: `vtHImJ7kC0MNoLwlBImMx1Fu3XCw59nn`,
+  WSI_ML_IMAGE_SEARCH_URL: `http://20.96.81.152:80/api/v1/service/sim-dev-cluster/score`,
+  WSI_ML_IMAGE_SEARCH_API_KEY: `9MJN5CSlwzlujzLEjZeCCZA1w4grf9xp`,
 
   WSI_ML_IMAGE_YOLO_URL: `http://20.102.12.78:80/api/v1/service/yolo-dev-cluster/score`,
   WSI_ML_IMAGE_YOLO_API_KEY: `Ea73FTpgaAOWVcjCkmtAWAmbeyUbIYMA`,
